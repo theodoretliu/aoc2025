0cc27721c6bcdd16ecded680874d405c6880aab4
Teddy Liu <theodoretliu@gmail.com>
2025-12-01 12:46:52 -0800

    initial

---
+++ day01.rb
@@ -0,0 +1,57 @@
+s = File.read("input.txt")
+
+rows = s.split.map(&:strip)
+
+dial = 50
+
+score = 0
+
+puts rows.length
+
+rows.each do |row|
+    num = row[1...].to_i
+    if row[0] == "R"
+        dial += num
+    else
+        dial -= num
+    end
+
+    dial %= 100
+
+    if dial == 0
+        score +=1
+    end
+end
+
+puts score
+
+dial = 50
+clicks = 0
+
+rows.each do |row|
+    num = row[1...].to_i
+
+    if row[0] == "R"
+        num.times do 
+            dial += 1
+            dial %= 100
+
+            if dial == 0
+                clicks += 1
+            end
+        end
+    else
+        num.times do 
+            dial -= 1
+            dial %= 100
+
+            if dial == 0
+                clicks += 1
+            end
+        end
+    end
+
+    dial %= 100
+end
+
+puts clicks
